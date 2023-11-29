b0057f56a8da9b742b82d0756eee1822f49603f7
facundolcolavini <fcolavini@southernhorizon.tech>
2023-11-29 09:26:15 -0300

    adapter for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [tailwind(), preact()],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
