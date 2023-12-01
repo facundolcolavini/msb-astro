21ff0fcd4fc39e3f4b8b4bc9c758ba3b879df298
facundolcolavini <fcolavini@southernhorizon.tech>
2023-12-01 11:16:49 -0300

    Add API endpoint and card component

---
+++ src/pages/api/get-results-records.ts
@@ -0,0 +1,14 @@
+import type { APIResponseResultsRecords, Result } from "../../types/results.records.types";
+import { fetchData } from "../../utils/fetch-data";
+const endpoint = 'resultados.fichas';
+
+export const getResultsRecords = async (queryParams :any):Promise<Result> => {
+    try {
+        const res = await fetchData(endpoint, queryParams) as APIResponseResultsRecords;
+        return res.resultado;
+
+    } catch (error) {
+        console.error('Error fetching entrepreneurship data:', error);
+        throw error;
+    }
+}
