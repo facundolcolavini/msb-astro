0c013f46d01b80b648d1d85606a5731022719f69
Facundo Colavini <fcolavini@southernhorizon.tec>
2023-12-07 12:57:26 -0300

    Remove commented out code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify"; 
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
