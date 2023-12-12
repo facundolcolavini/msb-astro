b7d3556c9269454815fba237989e4275f54dfb36
facundolcolavini <fcolavini@southernhorizon.tech>
2023-12-12 19:32:04 -0300

    #server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'hybrid',
+  output: 'server',
   adapter: netlify({
     edgeMiddleware: true
   }),
