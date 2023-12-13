1cfdb88892cbaea32fcd536b626be8a77942f7a6
facundolcolavini <fcolavini@southernhorizon.tech>
2023-12-13 16:33:16 -0300

    #Add filter on formfilter

--- src/components/preact/FormFilters.tsx
+++ src/components/preact/FormFilters.tsx
@@ -8,14 +8,15 @@ import FilterSelect from "./FilterSelect";
 
 interface Props {
   selects: Results
-  onFilter?: (e: Record<string, string>) => void
+
 }
 
-const FormFilters = ({ selects, onFilter }: Props) => {
-  const { localidades: sellocalidades, barrio: barrios1, operacion: tipo_operacion, tipo: tipo_inmueble, ambientes:Ambientes, calles:Calles } = selects
+const FormFilters = ({ selects }: Props) => {
+  const { localidades: sellocalidades, barrio: barrios1, operacion: tipo_operacion, tipo: tipo_inmueble, ambientes: Ambientes, calles, valor } = selects
   // Obtener los valores de los filtros de la URL 
   const [urlSearchParams, setUrlSearchParams] = useState({});
   const [select, setSelect] = useState({});
+  const { desde, hasta } = valor;
 
   const bar = barrios1.filter(
     ({ descripcion }: Option) => descripcion !== "INDISTINTO"
@@ -40,22 +41,51 @@ const FormFilters = ({ selects, onFilter }: Props) => {
   ).map(({ descripcion, value }: Option) => (
     { label: capitalize(descripcion.toLowerCase()), value }
   ));
-    
+
   const amb = Ambientes.filter(
     ({ descripcion }: Option) => descripcion !== "INDISTINTO"
   ).map(({ descripcion, value }: Option) => (
     { label: capitalize(descripcion.toLowerCase()), value }
   ));
 
-  const cal = Calles.filter(
+  const cal = calles.filter(
     ({ descripcion }: Option) => descripcion !== "INDISTINTO"
   ).map(({ descripcion, value }: Option) => (
     { label: capitalize(descripcion.toLowerCase()), value }
   ));
 
+  const moneda = [
+    { label: "$", value: "P" },
+    { label: "U$S", value: "D" },
+  ]
+
+  // Min and max values 
+  const min = desde.filter(
+    ({ descripcion }) => descripcion !== "INDISTINTO"
+  ).map(({ descripcion, val: value }) => (
+    { label: capitalize(descripcion.toLowerCase()), value }
+  ));
+
+  const max = hasta.filter(
+    ({ descripcion }) => descripcion !== "INDISTINTO"
+  ).map(({ descripcion, val: value }) => (
+    { label: capitalize(descripcion.toLowerCase()), value }
+  ));
+
   const handleSelect: JSX.GenericEventHandler<HTMLElement> = (e) => {
     const { id, value } = e.target as HTMLSelectElement;
-    setSelect({ ...select, [id]: value });
+    if (id === "valor_minimo" || id === "valor_maximo") {
+      if (value) {
+        setSelect({ ...select, [id]: Number(value) });
+
+        // Update URL with minPrice and maxPrice
+        const searchParams = new URLSearchParams(window.location.search);
+        searchParams.set(id, value);
+        window.history.pushState({}, '', '?' + searchParams.toString());
+      }
+    } else {
+      setSelect({ ...select, [id]: value });
+    }
   };
 
   // Actualizar los filtros de la URL para que se reflejen en la tabla
@@ -106,21 +136,6 @@ const FormFilters = ({ selects, onFilter }: Props) => {
 
     <form id="form-filters" class="mb-4 w-100" onSubmit={hanldeSubmit}>
       <div class="grid grid-rows md:grid-cols-1 gap-2">
-        <label htmlFor="sellocalidades" class="text-white">Localidad :</label>
-        <FilterSelect
-          id="sellocalidades"
-          opts={locs}
-          onChange={handleSelect}
-          defaultOption={{ value: "All", label: "Seleccione una localidad" }}
-        />
-
-        <label class="text-white" htmlFor="barrios1" id={"barrios1"}>Barrios:     </label>
-        <FilterSelect
-          id="barrios1"
-          opts={bar}
-          onChange={handleSelect}
-          defaultOption={{ value: "All", label: "Seleccione un barrio" }}
-        />
         <label class="text-white" htmlFor="tipo_operacion" id={"tipo_operacion"}>Operación:</label>
         <FilterSelect
           id="tipo_operacion"
@@ -143,20 +158,54 @@ const FormFilters = ({ selects, onFilter }: Props) => {
           onChange={handleSelect}
           defaultOption={{ value: "All", label: "Seleccione los Ambientes" }}
         />
+        <label htmlFor="sellocalidades" class="text-white">Localidad :</label>
+        <FilterSelect
+          id="sellocalidades"
+          opts={locs}
+          onChange={handleSelect}
+          defaultOption={{ value: "All", label: "Seleccione una localidad" }}
+        />
 
-<label class="text-white" htmlFor="calles" id={"calles"}>Calle:</label>
+        <label class="text-white" htmlFor="barrios1" id={"barrios1"}>Barrios:     </label>
+        <FilterSelect
+          id="barrios1"
+          opts={bar}
+          onChange={handleSelect}
+          defaultOption={{ value: "All", label: "Seleccione un barrio" }}
+        />
+        <label class="text-white" htmlFor="calles" id={"calles"}>Calle:</label>
         <FilterSelect
           id="calles"
           opts={cal}
           onChange={handleSelect}
           defaultOption={{ value: "All", label: "Seleccione los Ambientes" }}
         />
+        <label class="text-white" htmlFor="moneda" id={"moneda"}>Moneda:</label>
+        <FilterSelect
+          id="moneda"
+          opts={moneda}
+          onChange={handleSelect}
+          defaultOption={{ value: "All", label: "Seleccione la Moneda" }}
+        />
+
+
       </div>
+      <div class="grid grid-flow-col my-2">
+        <span class="text-white flex items-center ">Valor:</span>
+        <FilterSelect
+          id="valor_minimo"
+          opts={min}
+          onChange={handleSelect}
+          defaultOption={{ value: "All", label: "Minimo" }}
+        />
 
-      <div class="grid grid-rows md:grid-cols-1 my-2 gap-2">
-
+        <FilterSelect
+          id="valor_maximo"
+          opts={max}
+          onChange={handleSelect}
+          defaultOption={{ value: "All", label: "Maximo" }}
+        />
       </div>
-
 
       <div class="flex justify-between items-center px-1 mt-5 gap-1">
         <button class="bg-[#768294] text-white px-4 py-2 rounded" type="button" onClick={handleReset}>Limpiar</button>
