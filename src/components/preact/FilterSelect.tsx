2823ce23f235225e336385d8c19186ce0f41c52e
Facundo Colavini <fcolavini@southernhorizon.tec>
2023-12-19 15:53:25 -0300

    Refactor FilterSelect component styles

--- src/components/preact/FilterSelect.tsx
+++ src/components/preact/FilterSelect.tsx
@@ -43,32 +43,32 @@ const FilterSelect = ({ opts, id, onChange, defaultOption }: FilterSelectProps):
   }, []);
 
   return (
-    <div ref={dropdownRef} class="relative w-100 m-1">
+    <div ref={dropdownRef} className="relative w-100 m-1">
       <button
         id={id}
         onClick={toggleDropdown}
-        class="w-full p-2 rounded text-white flex bg-gray-600 justify-between items-center"
+        className="w-full p-2 rounded text-white flex bg-gray-600 justify-between items-center"
         type="button"
       >
         {selectedOption || 'Select Option'}
         <svg
-          class={`w-4 h-4 ms-3 ${isOpen ? 'transform rotate-180' : ''}`}
+          className={`w-4 h-4 ms-3 ${isOpen ? 'transform rotate-180' : ''}`}
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 10 6"
         >
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+          <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
         </svg>
       </button>
 
       {isOpen && (
-        <div class="w-full block absolute z-10 bg-white rounded-lg shadow max-h-32 overflow-y-auto">
+        <div className="w-full block absolute z-10 bg-white rounded-lg shadow max-h-32 overflow-y-auto">
           {opts.map((option, index) => (
             <button
               key={index}
               onClick={() => handleOptionClick(option)}
-              class="w-full  text-left p-2 hover:bg-gray-100 animate-fadeIn"
+              className="w-full  text-left p-2 hover:bg-gray-100 animate-fadeIn"
             > 
               {option.label }
             </button>
