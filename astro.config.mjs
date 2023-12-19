5668ccd8186b5468bb4d54945d59c92132d9b8d1
Facundo Colavini <fcolavini@southernhorizon.tec>
2023-12-19 16:27:20 -0300

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import netlify from '@astrojs/netlify';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "hybrid",
-  adapter: netlify(),
+  adapter: netlify()
 });
