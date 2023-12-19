294842ed85b93636319d1770e4a46d004c85c964
Facundo Colavini <fcolavini@southernhorizon.tec>
2023-12-19 16:56:59 -0300

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import netlify from '@astrojs/netlify';
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
-
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    // Example:
+   routes: ['/', '/emprendimientos', '/barrios-cerrados-countries']
+  
+
+  },
   integrations: [tailwind(), preact()],
-  output: "hybrid",
+  output: 'hybrid',
   adapter: netlify()
 });
