1a3e2ea5de6ae19c645d277ebc0289cc307069f8
Facundo Colavini <fcolavini@southernhorizon.tec>
2023-12-19 16:33:30 -0300

    Remove duplicate import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import netlify from '@astrojs/netlify';
 
-import netlify from "@astrojs/netlify/functions";
+
 
 // https://astro.build/config
 export default defineConfig({
